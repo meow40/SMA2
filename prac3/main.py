d1f8082a20de6e3168b17286eff376293ce21055
Shobhit Mirjankar <shobhitmir@gmail.com>
2023-04-24 22:35:41 +0530

    Added trendline

--- prac3/main.py
+++ prac3/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+import numpy as np
 
 dataset = pd.read_csv('tweets.csv')
 dataset = dataset[['created_at','tweet']]
@@ -24,7 +26,14 @@ plt.ylabel('No. of Tweets')
 plt.title('Tweet Activity based on hour of day')
 plt.show()
 
-plt.plot(date_freq.index,date_freq.values, '-o')
+plt.scatter(date_freq.index,date_freq.values)
+x = date_freq.index
+x_num = dates.date2num(x) # We cannot plot trendline using datetime object, so convert date to number
+y = date_freq.values
+z = np.polyfit(x_num,y,deg=1)
+p = np.poly1d(z)
+
+plt.plot(x, p(x_num))
 plt.xlabel('Date when tweeted')
 plt.ylabel('No. of Tweets')
 plt.title('Tweet Trend')
